62c9b928cd02b15a568244ebcfc8c97dd8598253
abayer <andrew.bayer@gmail.com>
2010-03-08 12:10:27 -0800

    Initial commit

---
+++ src/main/java/org/codehaus/mojo/cobertura/AbstractCoberturaMojo.java
@@ -0,0 +1,108 @@
+package org.codehaus.mojo.cobertura;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.mojo.cobertura.configuration.ConfigCheck;
+import org.codehaus.mojo.cobertura.configuration.ConfigInstrumentation;
+import org.codehaus.mojo.cobertura.tasks.AbstractTask;
+
+/**
+ * Abstract Base for Cobertura Mojos.
+ * 
+ * @author <a href="mailto:joakim@erdfelt.com">Joakim Erdfelt</a>
+ */
+public abstract class AbstractCoberturaMojo
+    extends AbstractMojo
+{
+    /**
+     * <i>Maven Internal</i>: Project to interact with.
+     * 
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    protected MavenProject project;
+
+    /**
+     * Maximum memory to pass JVM of Cobertura processes.
+     * 
+     * @parameter expression="${cobertura.maxmem}"
+     */
+    private String maxmem = "64m";
+
+    /**
+     * <p>
+     * The Datafile Location.
+     * </p>
+     * 
+     * @parameter expression="${cobertura.datafile}" default-value="${project.build.directory}/cobertura/cobertura.ser"
+     * @required
+     * @readonly
+     */
+    protected File dataFile;
+
+    /**
+     * The <a href="usage.html#Check">Check Configuration</a>.
+     * 
+     * @parameter expression="${check}"
+     */
+    protected ConfigCheck check;
+
+    /**
+     * The <a href="usage.html#Instrumentation">Instrumentation Configuration</a>.
+     * 
+     * @parameter expression="${instrumentation}"
+     */
+    protected ConfigInstrumentation instrumentation;
+
+    /**
+     * Only output coberura errors, avoid info messages.
+     * 
+     * @parameter expression="${quiet}" default-value="false"
+     */
+    private boolean quiet;
+
+    /**
+     * <i>Maven Internal</i>: List of artifacts for the plugin.
+     * 
+     * @parameter expression="${plugin.artifacts}"
+     * @required
+     * @readonly
+     */
+    protected List pluginClasspathList;
+
+    /**
+     * Setup the Task defaults.
+     * 
+     * @param task the task to setup.
+     */
+    public void setTaskDefaults( AbstractTask task )
+    {
+        task.setLog( getLog() );
+        task.setPluginClasspathList( pluginClasspathList );
+        task.setMaxmem( maxmem );
+        task.setQuiet( quiet );
+    }
+}
---
+++ src/main/java/org/codehaus/mojo/cobertura/CoberturaGenerateReportMojo.java
@@ -0,0 +1,93 @@
+package org.codehaus.mojo.cobertura;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import org.apache.maven.artifact.handler.ArtifactHandler;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.mojo.cobertura.tasks.GenerateReportTask;
+
+/**
+ * Cobertura report generation.
+ * 
+ * @author Witek Wołejszo ww@touk.pl
+ * @goal generate-report
+ */
+public class CoberturaGenerateReportMojo extends AbstractCoberturaMojo {
+
+    /**
+     * The format of the report. (can be 'html' or 'xml'. defaults to 'xml')
+     * 
+     * @parameter
+     */
+    private String outputFormat = "xml";
+    
+    /**
+     * The output directory for the report.
+     * 
+     * @parameter default-value="${project.build.directory}/site/cobertura"
+     * @required
+     */
+    private File outputDirectory;
+    
+    public void execute() throws MojoExecutionException {
+        
+        ArtifactHandler artifactHandler = project.getArtifact().getArtifactHandler();
+        
+        if (!"java".equals(artifactHandler.getLanguage())) {
+            getLog().info("Not executing cobertura:generate-report as the project is not a Java classpath-capable package");
+        } else {
+
+            if (!dataFile.exists()) {
+                getLog().info("Cannot perform generate-report, instrumentation not performed - skipping.");
+            } else {
+                GenerateReportTask task = new GenerateReportTask();
+                setTaskDefaults(task);
+                task.setDataFile(dataFile);
+                task.setOutputFormat(outputFormat);
+                task.setOutputDirectory(outputDirectory);
+                task.setCompileSourceRoots(project.getCompileSourceRoots());
+                task.execute();
+            }
+            
+            String orginalOutputDirectory = System.getProperty("orginal.project.build.outputDirectory");
+            getLog().info("Current output directory: " + System.getProperty("project.build.outputDirectory"));
+            getLog().info("Orginal output directory: " + orginalOutputDirectory);
+            getLog().info("Changing back to orginal directory.");
+            
+            if (orginalOutputDirectory != null) {
+                project.getBuild().setOutputDirectory(orginalOutputDirectory);
+                System.setProperty("project.build.outputDirectory", orginalOutputDirectory);
+                System.setProperty("orginal.project.build.outputDirectory" , "");
+            }
+            
+        }
+
+    }
+
+    public void setOutputDirectory(File outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    public File getOutputDirectory() {
+        return outputDirectory;
+    }
+}
